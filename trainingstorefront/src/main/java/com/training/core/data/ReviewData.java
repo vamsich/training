d1bc4169f71b3490aa985a864ad950691fc92b31
Vamsi <vamsi.chadaram@nttdata.com>
2018-03-16 20:54:02 +0530

    Review

---
+++ trainingstorefront/src/main/java/com/training/core/data/ReviewData.java
@@ -0,0 +1,28 @@
+package com.training.core.data;
+
+public class ReviewData 
+{
+	private Long id;
+	private String comment;
+	private Integer rating;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public Integer getRating() {
+		return rating;
+	}
+	public void setRating(Integer rating) {
+		this.rating = rating;
+	}
+	
+}
