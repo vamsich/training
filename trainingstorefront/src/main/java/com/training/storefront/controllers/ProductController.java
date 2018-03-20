56f8e0f92b784093304b2339226545573431b7f5
Vamsi <vamsi.chadaram@nttdata.com>
2018-03-20 17:13:03 +0530

    product controller

--- trainingstorefront/src/main/java/com/training/storefront/controllers/ProductController.java
+++ trainingstorefront/src/main/java/com/training/storefront/controllers/ProductController.java
@@ -1,16 +1,22 @@
 package com.training.storefront.controllers;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.training.core.data.ProductData;
 import com.training.core.model.ProductModel;
+import com.training.core.service.ProductService;
 
 @Controller
 @RequestMapping("/p")
 public class ProductController 
 {
+	@Resource(name="productService")
+	private ProductService productService;
+	
 	@RequestMapping(value="/create", method= RequestMethod.POST)
 	public void createProduct(ProductData productData)
 	{
@@ -18,5 +24,7 @@ public class ProductController
 		product.setName(productData.getName());
 		product.setPrice(productData.getPrice());
 		product.setDescription(productData.getDescription());
+		product.setPrice(productData.getPrice());
+		productService.createProduct(product);
 	}
 }
